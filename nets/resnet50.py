c450efe55c7f296d0ffd9ff132741360d5a6bf62
NonameAllen <15916782791@163.com>
2024-03-22 11:21:55 +0800

    为resnet增加注意力机制

--- nets/resnet50.py
+++ nets/resnet50.py
@@ -1,9 +1,153 @@
 import math
 
+import torch
 import torch.nn as nn
 from torch.hub import load_state_dict_from_url
 
-
+# RGA注意力机制
+class RGA_Module(nn.Module):
+    def __init__(self,in_channel,in_spatial,use_spatial=True,use_channel=True,
+        cha_ratio=8,spa_ratio=8,down_ratio=8):
+        super(RGA_Module,self).__init__()
+        self.in_channel=in_channel  # C=256
+        self.in_spatial=in_spatial  # H*W=64*32=2048
+        self.inter_channel=in_channel//cha_ratio    # C//8=256//8=32
+        self.inter_spatial=in_spatial//spa_ratio    # (H*W)//8=2048//8=256
+        self.use_spatial=use_spatial    # 是否使用RGA-S
+        self.use_channel=use_channel    # 是否使用RGA-C
+        if self.use_spatial:
+            # 定义5个卷积
+            # (8*256*64*32)--(8*32*64*32)
+            self.theta_spatial=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_channel,
+                            out_channels=self.inter_channel,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_channel),
+                    nn.ReLU()
+            )
+            # (8*256*64*32)--(8*32*64*32)
+            self.phi_spatial=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_channel,
+                            out_channels=self.inter_channel,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_channel),
+                    nn.ReLU()
+            )
+            # (8*4096*64*32)--(8*256*64*32)
+            self.gg_spatial=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_spatial*2,
+                            out_channels=self.inter_spatial,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_spatial),
+                    nn.ReLU()
+            )
+            # (8*256*64*32)--(8*32*64*32)
+            self.gx_spatial=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_channel,
+                            out_channels=self.inter_channel,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_channel),
+                    nn.ReLU()
+            )
+            # (8*257*64*32)--(8*1*64*32)
+            num_channel_s=1+self.inter_spatial
+            self.W_spatial=nn.Sequential(
+                    nn.Conv2d(in_channels=num_channel_s,
+                            out_channels=num_channel_s//down_ratio,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(num_channel_s//down_ratio),
+                    nn.ReLU(),
+                    nn.Conv2d(in_channels=num_channel_s//down_ratio,
+                            out_channels=1,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(1)
+            )
+        if self.use_channel:
+            # 定义5个卷积
+            # (8*2048*256*1)--(8*256*256*1)
+            self.theta_channel=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_spatial,
+                            out_channels=self.inter_spatial,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_spatial),
+                    nn.ReLU()
+            )
+            # (8*2048*256*1)--(8*256*256*1)
+            self.phi_channel=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_spatial,
+                            out_channels=self.inter_spatial,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_spatial),
+                    nn.ReLU()
+            )
+            # (8*512*256*1)--(8*32*256*1)
+            self.gg_channel=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_channel*2,
+                            out_channels=self.inter_channel,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_channel),
+                    nn.ReLU()
+            )
+            # (8*2048*256*1)--(8*256*256*1)
+            self.gx_channel=nn.Sequential(
+                    nn.Conv2d(in_channels=self.in_spatial,
+                            out_channels=self.inter_spatial,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(self.inter_spatial),
+                    nn.ReLU()
+            )
+            # (8*33*256*1)--(8*1*256*1)
+            num_channel_c=1+self.inter_channel
+            self.W_channel=nn.Sequential(
+                    nn.Conv2d(in_channels=num_channel_c,
+                            out_channels=num_channel_c//down_ratio,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(num_channel_c//down_ratio),
+                    nn.ReLU(),
+                    nn.Conv2d(in_channels=num_channel_c//down_ratio,
+                            out_channels=1,
+                            kernel_size=1,stride=1,padding=0,bias=False),
+                    nn.BatchNorm2d(1)
+            )
+    def forward(self,x):
+        # input x:(8,256,64,32)
+        b,c,h,w=x.size()    # 8,256,64,32
+        if self.use_spatial:
+            theta_xs=self.theta_spatial(x)  # 8*32*64*32
+            phi_xs=self.phi_spatial(x)      # 8*32*64*32
+            theta_xs=theta_xs.view(b,self.inter_channel,-1) # 8*32*2048
+            theta_xs=theta_xs.permute(0,2,1)                # 8*2048*32
+            phi_xs=phi_xs.view(b,self.inter_channel,-1)     # 8*32*2048
+            Gs=torch.matmul(theta_xs,phi_xs)                # 8*2048*2048
+            Gs_in=Gs.permute(0,2,1).view(b,h*w,h,w)         # 8*2048*64*32
+            Gs_out=Gs.view(b,h*w,h,w)                       # 8*2048*64*32
+            Gs_joint=torch.cat((Gs_in,Gs_out),1)            # 8*4096*64*32
+            Gs_joint=self.gg_spatial(Gs_joint)              # 8*256*64*32
+            g_xs=self.gx_spatial(x)                         # 8*32*64*32
+            g_xs=torch.mean(g_xs,dim=1,keepdim=True)        # 8*1*64*32
+            ys=torch.cat((g_xs,Gs_joint),1)                 # 8*257*64*32
+            w_ys=self.W_spatial(ys)                         # 8*1*64*32
+            if not self.use_channel:
+                out=torch.sigmoid(w_ys.expand_as(x))*x      # 8*256*64*32
+                return out
+            else:
+                x=torch.sigmoid(w_ys.expand_as(x))*x        # 8*256*64*32
+        if self.use_channel:
+            xc=x.view(b,c,-1).permute(0,2,1).unsqueeze(-1)              # 8*2048*256*1
+            theta_xc=self.theta_channel(xc).squeeze(-1).permute(0,2,1)  # 8*256*256
+            phi_xc=self.phi_channel(xc).squeeze(-1) # 8*256*256
+            Gc=torch.matmul(theta_xc,phi_xc)        # 8*256*256
+            Gc_in=Gc.permute(0,2,1).unsqueeze(-1)   # 8*256*256*1
+            Gc_out=Gc.unsqueeze(-1)                 # 8*256*256*1
+            Gc_joint=torch.cat((Gc_in,Gc_out),1)    # 8*512*256*1
+            Gc_joint=self.gg_channel(Gc_joint)      # 8*32*256*1
+            g_xc=self.gx_channel(xc)                # 8*256*256*1
+            g_xc=torch.mean(g_xc,dim=1,keepdim=True)    # 8*1*256*1
+            yc=torch.cat((g_xc,Gc_joint),1)             # 8*33*256*1
+            w_yc=self.W_channel(yc).transpose(1,2)      # 8*1*256*1--8*256*1*1
+            out=torch.sigmoid(w_yc)*x                   # 8*256*64*32
+            return out
+        
 class Bottleneck(nn.Module):
     expansion = 4
     def __init__(self, inplanes, planes, stride=1, downsample=None):
@@ -42,13 +186,14 @@ class Bottleneck(nn.Module):
 
         return out
 
-class ResNet(nn.Module):
-    def __init__(self, block, layers, num_classes=1000):
+class ResNet_RGA(nn.Module):
+    def __init__(self, block, layers, num_classes=1000,height=256,width=128,spa_on=True,
+                    cha_on=True,s_ratio=8,c_ratio=8,d_ratio=8 ):
         #-----------------------------------#
         #   假设输入进来的图片是600,600,3
         #-----------------------------------#
         self.inplanes = 64
-        super(ResNet, self).__init__()
+        super(ResNet_RGA, self).__init__()
 
         # 600,600,3 -> 300,300,64
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -66,7 +211,15 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         # self.layer4被用在classifier模型中
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        
+         # RGA注意力机制
+        self.rga1=RGA_Module(256,(height//4)*(width//4),use_spatial=spa_on,use_channel=cha_on,
+                                cha_ratio=c_ratio,spa_ratio=s_ratio,down_ratio=d_ratio)
+        self.rga2=RGA_Module(512,(height//8)*(width//8),use_spatial=spa_on,use_channel=cha_on,
+                                cha_ratio=c_ratio,spa_ratio=s_ratio,down_ratio=d_ratio)
+        self.rga3=RGA_Module(1024,(height//16)*(width//16),use_spatial=spa_on,use_channel=cha_on,
+                                cha_ratio=c_ratio,spa_ratio=s_ratio,down_ratio=d_ratio)
+        self.rga4=RGA_Module(2048,(height//16)*(width//16),use_spatial=spa_on,use_channel=cha_on,
+                                cha_ratio=c_ratio,spa_ratio=s_ratio,down_ratio=d_ratio)
         self.avgpool = nn.AvgPool2d(7)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -112,7 +265,7 @@ class ResNet(nn.Module):
         return x
 
 def resnet50(pretrained = False):
-    model = ResNet(Bottleneck, [3, 4, 6, 3])
+    model = ResNet_RGA(Bottleneck, [3, 4, 6, 3])
     if pretrained:
         state_dict = load_state_dict_from_url("https://download.pytorch.org/models/resnet50-19c8e357.pth", model_dir="./model_data")
         model.load_state_dict(state_dict)
